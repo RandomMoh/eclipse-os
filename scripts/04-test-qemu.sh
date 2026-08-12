120f9669700bfd592ad9e576444f42410519136c
RandomMoh <mohsinddop2309@gmail.com>
2026-08-13 00:51:49 +0900

    disable auto qemu launch

--- scripts/04-test-qemu.sh
+++ scripts/04-test-qemu.sh
@@ -30,4 +30,4 @@ else
 fi
 
 echo -e "${BLUE}[+] Launching Eclipse OS inside QEMU...${RESET}"
-qemu-system-x86_64 $KVM_ARG -m 4096 -smp 4 -vga virtio -display gtk -cdrom "$ISO_PATH" -boot d
+#qemu-system-x86_64 $KVM_ARG -m 4096 -smp 4 -vga virtio -display gtk -cdrom "$ISO_PATH" -boot d
